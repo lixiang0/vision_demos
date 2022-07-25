e89e433267fa901280b1aa3dd2862e5fa6d39ce8
xiaozhonghua <xiaozhonghua@exinova.cn>
2022-07-25 01:58:49 +0000

    add test code

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # vision_demos
 computer vision demo
+
+
+
+
+## test_pointnet
+
+### 1.model
+pure pytorch PointNet model
+
+### 2.dataset
+Download alignment **ModelNet** [here](https://shapenet.cs.stanford.edu/media/modelnet40_normal_resampled.zip) and save in `data/modelnet40_normal_resampled/`.
--- test_pointnet/datasets/modelnet40.py
+++ test_pointnet/datasets/modelnet40.py
@@ -1,6 +1,7 @@
 from turtle import distance
 from torch.utils.data import Dataset
-import os
+import os,pickle
+from tqdm import tqdm
 import numpy as np
 class Datasets(Dataset):
 
@@ -10,21 +11,45 @@ class Datasets(Dataset):
         self.num_catagory=num_catagory
         self.cat2id=self._load_catagory()
         self.id2cat=dict()
+        self.npoint=npoint
+        self.save_path='datasets/modelnet40'
         for key in self.cat2id:
             self.id2cat[self.cat2id[key]]=key
         self.train_list,self.test_list=self._load_id_list()
-        self.npoint=npoint
+        
     def _load_catagory(self):
         cat_path=os.path.join(self.root_path,f'modelnet{self.num_catagory}_shape_names.txt')
         cats=[ line.rstrip() for line in open(cat_path,'r').readlines()]
         cat2id=dict(zip(cats,range(len(cats))))
         return cat2id
     def _load_id_list(self):
-        train_path=os.path.join(self.root_path,f'modelnet{self.num_catagory}_train.txt')
-        test_path=os.path.join(self.root_path,f'modelnet{self.num_catagory}_test.txt')
-        train_list=[ line.rstrip() for line in open(train_path,'r').readlines()]
-        test_list=[ line.rstrip() for line in open(test_path,'r').readlines()]
-        return train_list,test_list
+        if not os.path.exists(self.save_path):
+            train_path=os.path.join(self.root_path,f'modelnet{self.num_catagory}_train.txt')
+            test_path=os.path.join(self.root_path,f'modelnet{self.num_catagory}_test.txt')
+            train_list=[ line.rstrip() for line in open(train_path,'r').readlines()]
+            test_list=[ line.rstrip() for line in open(test_path,'r').readlines()]
+            _train_list=[]
+            _test_list=[]
+            for i,flist in enumerate([train_list,test_list]):
+                for _,fid in tqdm(enumerate(flist),total=len(flist),smoothing=.9):
+                    index_path=os.path.join(self.root_path,'_'.join(fid.split('_')[0:-1]),fid+'.txt')
+                    points_set=np.loadtxt(index_path,delimiter=',').astype(np.float32).reshape(-1,6)[:,:3]
+                    # if self.preprocess:
+                    class_id=self.cat2id['_'.join(fid.split('_')[0:-1])]
+                    points_set=self._sample(points_set,self.npoint)
+                    points_set=self._norm(points_set)
+                    if i==0:
+                        _train_list.append((points_set,class_id))
+                    else:
+                        _test_list.append((points_set,class_id))
+            
+            with open(self.save_path, 'wb') as f:
+                pickle.dump([_train_list, _test_list], f)
+            return _train_list,_test_list
+        else:
+            with open(self.save_path, 'rb') as f:
+                _train_list,_test_list=pickle.load(f)
+            return _train_list,_test_list
     
     def __len__(self):
         if self.split is 'train':
@@ -54,15 +79,18 @@ class Datasets(Dataset):
         return point_set
     def _getitem(self,index):
         if self.split is not 'train':
-            fid=self.test_list[index]
+            (points_set,class_id)=self.test_list[index]
         else:
-            fid=self.train_list[index]
-        # print('index:',index,fid)
-        index_path=os.path.join(self.root_path,'_'.join(fid.split('_')[0:-1]),fid+'.txt')
-        points_set=np.loadtxt(index_path,delimiter=',').astype(np.float32).reshape(-1,6)[:,:3]
-        # if self.preprocess:
-        class_id=self.cat2id['_'.join(fid.split('_')[0:-1])]
-        points_set=self._sample(points_set,self.npoint)
+            (points_set,class_id)=self.train_list[index]
+        # # print('index:',index,fid)
+        # index_path=os.path.join(self.root_path,'_'.join(fid.split('_')[0:-1]),fid+'.txt')
+        # points_set=np.loadtxt(index_path,delimiter=',').astype(np.float32).reshape(-1,6)[:,:3]
+        # # if self.preprocess:
+        # class_id=self.cat2id['_'.join(fid.split('_')[0:-1])]
+        # points_set=self._sample(points_set,self.npoint)
+        # points_set=self._norm(points_set)
+        # points_set=points_set.reshape(-1,6)[:,:3]
+        # points_set=self._sample(points_set,self.npoint)
         # points_set=self._norm(points_set)
         return points_set.transpose(1,0),class_id
     def __getitem__(self,index):
@@ -71,8 +99,8 @@ class Datasets(Dataset):
 
 if __name__ == "__main__":
     import torch
-    dataset=Datasets('/home/ruben/datasets/modelnet40_normal_resampled')
-    dataloader=torch.utils.data.DataLoader(dataset,batch_size=1,shuffle=True)
+    dataset=Datasets('./datasets/modelnet40_normal_resampled')
+    dataloader=torch.utils.data.DataLoader(dataset,batch_size=2,shuffle=True)
     for point,label in dataloader:
         print(point.shape,point[:10],label)
         # print(dataset.id2cat[label.item()])
